5b66f8775f12fff5aa0677f46a628f6e24081a6f
Travis Rigg <rigg.travis@gmail.com>
2018-03-10 16:00:50 -0500

    Didn't add new files in last push

---
+++ src/main/java/dataobjects/BadTransactionException.java
@@ -0,0 +1,10 @@
+package dataobjects;
+
+public class BadTransactionException extends Exception {
+
+	/**
+	 * Exceptions must have Serial Version UIDs in order to be serialized properly
+	 */
+	private static final long serialVersionUID = 8814979071314494177L;
+
+}
