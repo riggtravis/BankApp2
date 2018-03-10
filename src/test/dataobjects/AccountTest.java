bb6200de270dcba902073f2b93bc8ee728e97e89
Travis Rigg <rigg.travis@gmail.com>
2018-03-09 23:23:24 -0500

    wrote account test

--- src/test/dataobjects/AccountTest.java
+++ src/test/dataobjects/AccountTest.java
@@ -1,14 +1,59 @@
 package dataobjects;
 
+import com.sun.corba.se.spi.orbutil.fsm.TestAction1;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
-
 public class AccountTest {
+	Account testAccount;
+
+	@Before
+	public void beforeEach () {
+		testAccount = new Account();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetAccounid() {
+		assertEquals(0, testAccount.getAccountid());
+	}
+
+	@Test
+	public void testSetAccountid() {
+		testAccount.setStatus(1);
+		assertEquals(1, testAccount.getAccountid());
+	}
+
+	@Test
+	public void testGetBalance() {
+		assertEquals((Double) 0.0D, (Double) testAccount.getBalance());
+	}
+
+	@Test
+	public void testSetBalance() {
+		testAccount.setBalance(0.1D);
+		assertEquals((Double) 0.1D, (Double) testAccount.getBalance());
+	}
+
+	@Test
+	public void testGetStatus() {
+		assertEquals(0, testAccount.getStatus());
+	}
+
+	@Test
+	public void testSetStatus() {
+		testAccount.setStatus(1);
+		assertEquals(0, testAccount.getStatus());
+	}
+
+	@Test
+	public void testGetAccounttype() {
+		assertEquals(0, testAccount.getAccounttype());
+	}
+
+	@Test
+	public void testSetAccounttype() {
+		testAccount.setAccounttype(1);
+		assertEquals(1, testAccount.getAccounttype());
 	}
 
 }
