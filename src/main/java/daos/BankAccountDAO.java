846212fb8d04ff5cd955dcd716f586b53751a990
Travis Rigg <rigg.travis@gmail.com>
2018-03-10 18:55:18 -0500

    Network issues. Can't test db interactions. Switching appartments

--- src/main/java/daos/BankAccountDAO.java
+++ src/main/java/daos/BankAccountDAO.java
@@ -2,6 +2,7 @@ package daos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
@@ -11,12 +12,18 @@ import utlities.ConnectionFactory;
 
 public class BankAccountDAO {
   final static Logger logger = Logger.getLogger(ConnectionFactory.class);
+  private String sql;
+  private Connection conn = ConnectionFactory.getInstance().getConnection();
 
-  // Create
-  public void createBankAccount(BankAccount account) {
-    String sql = "INSERT INTO ACCOUNT (BALANCE, STATUS, ACCOUNTTYPE) VALUES (?, ?, ?)";
-
-    Connection conn = ConnectionFactory.getInstance().getConnection();
+  /**
+   * Create Bank Account Function
+   * @param BankAccount account
+   *  This is the account that needs to be inserted into the database
+   * @return an integer representing the number of rows that have been inserted (should
+   * be 1)
+   */
+  public int createBankAccount(BankAccount account) {
+    sql = "INSERT INTO ACCOUNT (BALANCE, STATUS, ACCOUNTTYPE) VALUES (?, ?, ?)";
 
     try {
       conn.setAutoCommit(false);
@@ -24,15 +31,34 @@ public class BankAccountDAO {
       ps.setDouble(1, account.getBalance());
       ps.setInt(2, account.getStatus());
       ps.setInt(3, account.getAccounttype());
+      return ps.executeUpdate();
     } catch (SQLException e) {
       // I don't really know why this would happen
       logger.fatal(e);
+      return 0;
     }
   }
 
   // Read
   public BankAccount readBankAccount(int bankAccountid) {
-    return new BankAccount();
+    sql = "SELECT * FROM ACCOUNT WHERE BANK_ACCOUNT = ?";
+    BankAccount returnAccount = new BankAccount();
+    try {
+      PreparedStatement ps = conn.prepareStatement(sql);
+      ps.setInt(1, bankAccountid);
+      ResultSet rs = ps.executeQuery();
+      if (rs.next()) {
+        logger.info("Got an account");
+        returnAccount.setAccounttype(rs.getInt("accountType"));
+        returnAccount.setBalance(rs.getDouble("balance"));
+        returnAccount.setBankAccountid(rs.getInt("bankAccountID"));
+        returnAccount.setStatus(rs.getInt("status"));
+      }
+    } catch (SQLException e) {
+      // This happening seems bad. I don't know what causes it.
+      logger.fatal(e);
+    }
+    return returnAccount;
   }
 
 }
--- src/test/daos/BankAccountDAOTest.java
+++ src/test/daos/BankAccountDAOTest.java
@@ -2,30 +2,51 @@ package daos;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Savepoint;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import dataobjects.BankAccount;
+import utlities.ConnectionFactory;
+
 public class BankAccountDAOTest {
-  private final conn = ConnectionFactory.getInstance.getConnection();
+  private final Connection conn = ConnectionFactory.getInstance().getConnection();
   private Savepoint s;
   private BankAccount testAccount;
+  final static Logger logger = Logger.getLogger(ConnectionFactory.class);
 
   @Before
   public void setUp() {
-    BankAccount = new BankAccount(1, 1.0, 1, 1);
-    s = conn.setSavepoint("testSavepoint");
+    testAccount = new BankAccount(1, 1.0, 1, 1);
+    try {
+      s = conn.setSavepoint("testSavepoint");
+    } catch (SQLException e) {
+      // An exception during the set up for unit tests I think should be considered "bad"
+      logger.fatal(e);
+    }
   }
 
   @After
   public void tearDown() {
-    conn.rollback(s);
+    try {
+      conn.rollback(s);
+    } catch (SQLException e) {
+      // This should also be put into the category of "bad"
+      logger.fatal(e);
+    }
   }
 
   @Test
   public void testCreateBankAccountAndReadBankAccount() {
     BankAccountDAO dao = new BankAccountDAO();
     dao.createBankAccount(testAccount);
-    BankAccountDAO checkAccount = dao.readBankAccount(1);
-    assertEquals(testAccount, checkAccount);
+    BankAccount checkAccount = dao.readBankAccount(1);
+    assertEquals((Double) testAccount.getBalance(), (Double) checkAccount.getBalance());
   }
 
 }
