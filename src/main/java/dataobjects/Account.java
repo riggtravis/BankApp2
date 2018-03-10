43304653da1ce5cdc6f18c07339b7d4aa530eacc
Travis Rigg <rigg.travis@gmail.com>
2018-03-09 23:23:24 -0500

    wrote account test

--- src/main/java/dataobjects/Account.java
+++ src/main/java/dataobjects/Account.java
@@ -5,35 +5,42 @@ public class Account {
 	private double balance;
 	private int status;
 	private int accounttype;
-	
+
+	public Account () {
+		this.accountid		= 0;
+		this.balance			= 0;
+		this.status 			= 0;
+		this.accounttype	= 0;
+	}
+
 	public int getAccountid() {
 		return accountid;
 	}
-	
+
 	public void setAccountid(int accountid) {
 		this.accountid = accountid;
 	}
-	
+
 	public double getBalance() {
 		return balance;
 	}
-	
+
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
-	
+
 	public int getStatus() {
 		return status;
 	}
-	
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
-	
+
 	public int getAccounttype() {
 		return accounttype;
 	}
-	
+
 	public void setAccounttype(int accounttype) {
 		this.accounttype = accounttype;
 	}
