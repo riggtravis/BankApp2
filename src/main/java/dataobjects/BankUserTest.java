32bfc31e7d8496ec308583a1aa1779299d6e7653
Travis Rigg <rigg.travis@gmail.com>
2018-03-11 19:13:18 -0400

    Created a test file for BankUsers

---
+++ src/main/java/dataobjects/BankUserTest.java
@@ -0,0 +1,21 @@
+/**
+ * These are the tests to ensure the correctness of the BankUser class
+ */
+package dataobjects;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Travis Rigg
+ *
+ */
+public class BankUserTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
