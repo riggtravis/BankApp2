70f477ecfca98a539a7d5537d83de49c15aec8c4
Travis Rigg <rigg.travis@gmail.com>
2018-03-11 18:57:34 -0400

    Implemented the BankUser class

--- src/main/java/dataobjects/BankUser.java
+++ src/main/java/dataobjects/BankUser.java
@@ -1,5 +1,40 @@
 package dataobjects;
 
 public class BankUser {
+  private int bankUserID;
+  private String username;
+  private String userPassword;
+  private int userType;
 
+  public int getBankUserID () {
+    return this.bankUserID;
+  }
+
+  public void setBankUserID (int newUserID) {
+    this.bankUserID = newUserID;
+  }
+
+  public String getUsername () {
+    return this.username;
+  }
+
+  public void setUsername (String newUsername) {
+    this.username = newUsername;
+  }
+
+  public String getPassword () {
+    return this.userPassword;
+  }
+
+  public void setPassword (String newPassword) {
+    this.userPassword = newPassword;
+  }
+
+  public int getUserType () {
+    return this.userType;
+  }
+
+  public void setUserType (int newType) {
+    this.userType = newType;
+  }
 }
