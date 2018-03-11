7acfe985e40879599711a30e54ee1a7d24bd2dd3
Travis Rigg <rigg.travis@gmail.com>
2018-03-11 00:36:41 -0500

    Working on testing accountDao. Having network issues. Going to bed

--- src/main/java/daos/BankAccountDAO.java
+++ src/main/java/daos/BankAccountDAO.java
@@ -13,7 +13,20 @@ import utlities.ConnectionFactory;
 public class BankAccountDAO {
   final static Logger logger = Logger.getLogger(ConnectionFactory.class);
   private String sql;
-  private Connection conn = ConnectionFactory.getInstance().getConnection();
+  private final boolean PRODUCTION_VALUE = true;
+  private final Connection conn =
+      ConnectionFactory.getInstance().getConnection(PRODUCTION_VALUE);
+  private PreparedStatement ps;
+  
+  public BankAccountDAO() {
+    super();
+    try {
+      conn.setAutoCommit(false);
+    } catch (SQLException e) {
+      // I don't really know why this would happen
+      logger.fatal(e);
+    }
+  }
 
   /**
    * Create Bank Account Function
@@ -23,11 +36,10 @@ public class BankAccountDAO {
    * be 1)
    */
   public int createBankAccount(BankAccount account) {
-    sql = "INSERT INTO ACCOUNT (BALANCE, STATUS, ACCOUNTTYPE) VALUES (?, ?, ?)";
+    sql = "INSERT INTO BANK_ACCOUNT (BALANCE, STATUS, ACCOUNTTYPE) VALUES (?, ?, ?)";
 
     try {
-      conn.setAutoCommit(false);
-      PreparedStatement ps = conn.prepareStatement(sql);
+      ps = conn.prepareStatement(sql);
       ps.setDouble(1, account.getBalance());
       ps.setInt(2, account.getStatus());
       ps.setInt(3, account.getAccounttype());
@@ -41,24 +53,51 @@ public class BankAccountDAO {
 
   // Read
   public BankAccount readBankAccount(int bankAccountid) {
-    sql = "SELECT * FROM ACCOUNT WHERE BANK_ACCOUNT = ?";
+    sql = "SELECT * FROM BANK_ACCOUNT WHERE BANK_ACCOUNTID = ?";
     BankAccount returnAccount = new BankAccount();
     try {
-      PreparedStatement ps = conn.prepareStatement(sql);
+      ps = conn.prepareStatement(sql);
       ps.setInt(1, bankAccountid);
       ResultSet rs = ps.executeQuery();
       if (rs.next()) {
         logger.info("Got an account");
         returnAccount.setAccounttype(rs.getInt("accountType"));
         returnAccount.setBalance(rs.getDouble("balance"));
-        returnAccount.setBankAccountid(rs.getInt("bankAccountID"));
+        returnAccount.setBankAccountid(rs.getInt("bank_accountID"));
         returnAccount.setStatus(rs.getInt("status"));
+      } else {
+        logger.info("Did not get an account");
       }
     } catch (SQLException e) {
       // This happening seems bad. I don't know what causes it.
       logger.fatal(e);
     }
     return returnAccount;
+  }
+  
+  /**
+   * Update function
+   * @param an account (with a valid id) to be updated
+   * @return the number of rows that were updated
+   */
+  public int updateBankAccount(BankAccount updateAccount) {
+    StringBuilder sqlStringBuilder = new StringBuilder();
+    sqlStringBuilder.append("UPDATE BANK_ACCOUNT");
+    sqlStringBuilder.append("SET ACCOUNTTYPE = ?, BALANCE = ?, STATUS = ?");
+    sqlStringBuilder.append("WHERE BANK_ACCOUNTID = ?");
+    sql = sqlStringBuilder.toString();
+    try {
+      ps = conn.prepareStatement(sql);
+      ps.setInt(1, updateAccount.getAccounttype());
+      ps.setDouble(2, updateAccount.getBalance());
+      ps.setDouble(3, updateAccount.getStatus());
+      ps.setInt(4, updateAccount.getBankAccountid());
+      return ps.executeUpdate();
+    } catch (SQLException e) {
+      // This is the kind of thing that is considered "bad"
+      logger.fatal(e);
+      return 0;
+    }
   }
 
 }
--- src/test/daos/BankAccountDAOTest.java
+++ src/test/daos/BankAccountDAOTest.java
@@ -3,30 +3,49 @@ package daos;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Savepoint;
 
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import dataobjects.BankAccount;
 import utlities.ConnectionFactory;
 
 public class BankAccountDAOTest {
-  private final Connection conn = ConnectionFactory.getInstance().getConnection();
+  private final boolean PRODUCTION_VALUE = false;
+  private final Connection conn =
+      ConnectionFactory.getInstance().getConnection(PRODUCTION_VALUE);
   private Savepoint s;
   private BankAccount testAccount;
   final static Logger logger = Logger.getLogger(ConnectionFactory.class);
-
+  private BankAccountDAO dao;
+  
   @Before
   public void setUp() {
-    testAccount = new BankAccount(1, 1.0, 1, 1);
+    String sql = "DELETE FROM BANK_ACCOUNT";
+    StringBuilder sqlBuilder = new StringBuilder();
+    sqlBuilder.append("INSERT INTO BANK_ACCOUNT");
+    sqlBuilder.append("(BANK_ACCOUNTID, BALANCE, STATUS, ACCOUNTTYPE)");
+    sqlBuilder.append("VALUES (?, ?, ?, ?)");
+    testAccount = new BankAccount(2, 2.0, 2, 2);
     try {
+      PreparedStatement ps = conn.prepareStatement(sql);
+      ps.executeUpdate();
+      sql = sqlBuilder.toString();
+      ps = conn.prepareStatement(sql);
+      ps.setInt(1, 1);
+      ps.setDouble(2, 1.0D);
+      ps.setInt(3, 1);
+      ps.setInt(4, 1);
+      ps.executeUpdate();
       s = conn.setSavepoint("testSavepoint");
     } catch (SQLException e) {
-      // An exception during the set up for unit tests I think should be considered "bad"
+      // An exception during the set up for unit tests is considered "bad"
       logger.fatal(e);
     }
   }
@@ -40,13 +59,48 @@ public class BankAccountDAOTest {
       logger.fatal(e);
     }
   }
+  
+  @Test
+  public void testReadAccount() {
+    assertEquals(1, new BankAccountDAO().readBankAccount(1).getAccounttype());
+  }
 
+  /**
+   * This test currently only passes on an entirely new database. The possible solutions to
+   * this problem that I have come up with include the following:
+   *  Using the before class method(s) to create a completely clean database
+   *      PRO: Gauruntees that this test will be run correctly
+   *      CON: Clears out any data that we might have actually been using
+   *  Creating a special database for the tests to interact with and cleaning it out in the
+   *    before class methods
+   *      PRO: Gauruntees that this test will be run correctly
+   *      CON: Setting up databases is *hard*
+   *  Using other functions that will be created later on to back test this functionality
+   *      PRO: Requires no immediate action
+   *        Will work (eventually)
+   *      CON: Is not unit testing
+   *  Creating a test entry in our database to run tests on
+   *      PRO: Technically works
+   *      CON: Gross
+   *        Seems bad
+   *        Probably gonna do it anyway because I'm a bad human being
+   *  Just leaving everything as is
+   *      PRO: Requires no immediate or long term action
+   *      CON: Is annoying
+   */
   @Test
   public void testCreateBankAccountAndReadBankAccount() {
-    BankAccountDAO dao = new BankAccountDAO();
-    dao.createBankAccount(testAccount);
+    dao = new BankAccountDAO();
+    logger.info("Inserted " + dao.createBankAccount(testAccount) + " acccounts");
     BankAccount checkAccount = dao.readBankAccount(1);
     assertEquals((Double) testAccount.getBalance(), (Double) checkAccount.getBalance());
+  }
+  
+  @Test
+  public void testUpdateBankAccountAndReadBankAccount() {
+    dao = new BankAccountDAO();
+    dao.updateBankAccount(new BankAccount(1, 2.0D, 2, 2));
+    assertEquals((Double) 2.0D, (Double) dao.readBankAccount(1).getBalance());
   }
 
 }
