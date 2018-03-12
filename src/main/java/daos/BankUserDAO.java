1dd87d32fc732fcdad4a539bad744d674cd9e10e
Travis Rigg <rigg.travis@gmail.com>
2018-03-11 20:42:23 -0400

    Started writing a Bank User Dao

--- src/main/java/daos/BankUserDAO.java
+++ src/main/java/daos/BankUserDAO.java
@@ -1,5 +1,29 @@
 package daos;
 
 public class BankUserDAO {
+  final static Logger logger = Logger.getLogger(BankUserDAO.class);
+  private String sql;
+  private Connection conn;
 
+  /**
+   * Create
+   */
+  public int createBankUser(BankUser user) {
+    sql = "INSERT INTO BANK_USER"
+        + " (BANK_USERID, USERNAME, USER_PASSWORD, USER_TYPE)"
+        + " VALUES (?, ?, ?, ?)";
+    conn = ConnectionFactory.getInstance().getConnection();
+  }
+
+  /**
+   * Read
+   */
+
+  /**
+   * Update
+   */
+
+  /**
+   * Delete
+   */
 }
