6d4d4eca5607369039771fb094c81ff4b61a4b98
Travis Rigg <rigg.travis@gmail.com>
2018-03-12 19:48:33 -0400

    Time to implement the next part of the UI

--- src/test/ui/ConsoleUITest.java
+++ src/test/ui/ConsoleUITest.java
@@ -2,15 +2,21 @@ package ui;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Test;
 
 import daos.BankUserDAO;
 import dataobjects.BankUser;
+import utlities.ConnectionFactory;
 
 public class ConsoleUITest {
+  final static Logger logger = Logger.getLogger(ConsoleUITest.class);
 
   @Test
   public void testRegister() {
@@ -26,6 +32,18 @@ public class ConsoleUITest {
           dao.readBankUserByUserName("GreatUsername").getBankUserID());
     } catch (SQLException e) {
       fail("You didn't even get SQL right");
+    }
+  }
+  
+  @After
+  public void tearDown() {
+    Connection conn = ConnectionFactory.getInstance().getConnection();
+    try {
+      PreparedStatement ps = conn.prepareStatement(
+          "DELETE FROM BANK_USER WHERE USERNAME = 'GreatUser'");
+      ps.executeQuery();
+    } catch (SQLException e) {
+      logger.fatal(e);
     }
   }
 
