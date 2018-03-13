250920161031d4160ead9998dfa14ef8f5d2a0ea
Travis Rigg <rigg.travis@gmail.com>
2018-03-13 02:07:38 -0400

    join dao done

--- src/main/java/daos/UserToBankAccountDAO.java
+++ src/main/java/daos/UserToBankAccountDAO.java
@@ -6,11 +6,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Types;
 
+import org.apache.log4j.Logger;
+
 import dataobjects.BankAccount;
 import dataobjects.BankUser;
 import utlities.ConnectionFactory;
 
 public class UserToBankAccountDAO {
+  final static Logger logger = Logger.getLogger(UserToBankAccountDAO.class);
 
   public BankAccount createUserToBankAccountRelationship (
         BankUser owner, BankAccount account) throws SQLException {
@@ -18,10 +21,12 @@ public class UserToBankAccountDAO {
     PreparedStatement ps;
     BankAccountDAO accountDAO = new BankAccountDAO();
     String sql =
-      "INSERT INTO USER_TO_BANK_ACCOUNT (USERID, BANK_ACCOUNTID) VALUES (?, ?)";
+      "INSERT INTO USER_TO_BANK_ACCOUNT (BANK_USERID, BANK_ACCOUNTID) VALUES (?, ?)";
     int finalID;
     ps = conn.prepareStatement(sql);
+    logger.info("Prepared statement okay");
     ps.setInt(1, owner.getBankUserID());
+    logger.info("Successfully prepared the query for the user");
 
     /* Now we need to insert the bank account and get the ID that the database
      * assigns to it
@@ -30,14 +35,20 @@ public class UserToBankAccountDAO {
 
     // We should be able to get the most recent entry with the highest ID
     CallableStatement numberGetter =
-        conn.prepareCall("{? = CALL HIGHEST_BANK_ACCOUNT_ID}");
+        conn.prepareCall("{? = call HIGHEST_BANK_ACCOUNT_ID()}");
+    logger.info("Successfully prepared for function call");
     numberGetter.registerOutParameter(1, Types.INTEGER);
+    logger.info("Successfully loaded the function call");
     numberGetter.executeUpdate();
+    logger.info("Successfully called the function call");
 
     // Now we need to insert a relational entity into the database
     finalID = numberGetter.getInt(1);
-    ps.setInt(2, numberGetter.getInt(1));
+    logger.info("Successfully retrived a number");
+    ps.setInt(2, finalID);
+    logger.info("Succeeded in setting up the query for the account");
     ps.executeUpdate();
+    logger.info("Succeeded in updating the table");
     
     // Now return the bank account that was created
     return accountDAO.readBankAccount(finalID);
--- src/test/daos/UserToBankAccountDAOTest.java
+++ src/test/daos/UserToBankAccountDAOTest.java
@@ -2,33 +2,72 @@ package daos;
 
 import static org.junit.Assert.*;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Test;
 
+import dataobjects.BankAccount;
+import dataobjects.BankUser;
+import utlities.ConnectionFactory;
+
 public class UserToBankAccountDAOTest {
+  final static Logger logger = Logger.getLogger(UserToBankAccountDAOTest.class);
+  final static Connection conn = ConnectionFactory.getInstance().getConnection();
 
   @Test
   public void testCreateUserToBankAccountRelationship() {
     UserToBankAccountDAO testDAO = new UserToBankAccountDAO();
     BankUserDAO userDAO = new BankUserDAO();
     BankAccountDAO accountDAO = new BankAccountDAO();
-    Connection conn = ConnectionFactory.getInstance().getConnection();
 
     // Get a user to be the owner of the new account
-    BankUser testUser = userDAO.readBankUserByUserName("usr");
-    testDAO.createUserToBankAccountRelationship(
-        testUser, new BankAccount(0, 112.0D, 0, 1));
-
-    /* Make sure that the newest bank account and the bank account that was just
-     * inserted share the same VALUES
-     */
-    // Get the largest bankAccountid we can find
-    CallableStatement number getter =
-        conn.prepareCall("{? = CALL HIGHEST_BANK_ACCOUNT_ID}");
-    numberGetter.registerOutParameter(1, Types.INTEGER);
-    numberGetter.executeUpdate();
-
-    assertEquals(
-        (Double) 112.0D, accountDAO.readBankAccount(numberGetter.getInt(1)));
+    BankUser testUser;
+    try {
+      testUser = userDAO.readBankUserByUserName("usr");
+      logger.info("Got user!");
+      testDAO.createUserToBankAccountRelationship(
+          testUser, new BankAccount(0, 112.0D, 0, 1));
+      logger.info("Inserted data!");
+      /* Make sure that the newest bank account and the bank account that was just
+       * inserted share the same VALUES
+       */
+      // Get the largest bankAccountid we can find
+      CallableStatement numberGetter = conn.prepareCall(
+          "{? = call HIGHEST_BANK_ACCOUNT_ID()}");
+      logger.info("Getting ready to get the highest number!");
+      numberGetter.registerOutParameter(1, Types.INTEGER);
+      logger.info("Very ready to get the biggest number!");
+      numberGetter.executeUpdate();
+      logger.info("Got the biggest number!");
+      assertEquals(
+          (Double) 112.0D,
+          (Double) accountDAO.readBankAccount(
+              numberGetter.getInt(1)).getBalance());
+    } catch (SQLException e) {
+      fail("SQL errors abound!");
+    }
+  }
+  
+  @After
+  public void tearDown() {
+    try {
+      PreparedStatement ps = conn.prepareStatement(
+          "DELETE FROM BANK_ACCOUNT WHERE BANK_ACCOUNTID = ?");
+      CallableStatement numberGetter = conn.prepareCall(
+          "{? = call HIGHEST_BANK_ACCOUNT_ID()}");
+      numberGetter.registerOutParameter(1, Types.INTEGER);
+      numberGetter.executeUpdate();
+      ps.setInt(1, numberGetter.getInt(1));
+      ps.executeUpdate();
+    } catch (SQLException e) {
+      logger.fatal(e);
+    }
   }
 
 }
